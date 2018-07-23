9e659b08ac14da6eb0d9fc629e227e49c505f17c
이찬영 <dlcksdud3579@naver.com>
2018-07-23 20:27:02 +0900

    1. Music Player 서비스 추가(테스트 : 유희왕 오프닝 노래)

--- app/src/main/java/com/study/rev/androidrevolution/MainActivity.kt
+++ app/src/main/java/com/study/rev/androidrevolution/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.study.rev.androidrevolution
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -13,7 +14,19 @@ class MainActivity : AppCompatActivity() {
         val id = intent.getStringExtra("ID")
         val pw = intent.getStringExtra("PW")
 
-        t1.text = "당신의 ID는 " + id + "입니다."
-        t2.text = "비밀번호는 " + pw + "군요"
+        t1.text = "당신의 ID는 ${id}입니다."
+        t2.text = "비밀번호는 ${pw}군요"
+
+        btnPlay.setOnClickListener{
+            var intent = Intent(this, MusicService::class.java)
+            intent.putExtra("isPlay", true)
+            startService(intent)
+        }
+
+        btnStop.setOnClickListener {
+            var intent = Intent(this, MusicService::class.java)
+            intent.putExtra("isPlay", false)
+            startService(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/study/rev/androidrevolution/MusicService.kt
@@ -0,0 +1,30 @@
+package com.study.rev.androidrevolution
+
+import android.app.Service
+import android.content.Intent
+import android.graphics.Bitmap
+import android.media.MediaPlayer
+import android.os.IBinder
+
+class MusicService : Service() {
+    var player : MediaPlayer = MediaPlayer()
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        var isPlay = intent!!.getBooleanExtra("isPlay", false)
+
+        if (isPlay) {
+            player = MediaPlayer.create(this, R.raw.yugioh)
+            player.start()
+        }
+        else {
+            player.stop()
+            player.release()
+        }
+
+        return START_NOT_STICKY
+    }
+}
