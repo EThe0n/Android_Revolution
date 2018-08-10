beeb6b8154d2e249dcaa57889b463b1cb8217a3f
Etheon <snow_chris@naver.com>
2018-08-10 16:05:47 +0900

    세팅 프래그먼트에 로그아웃 기능 추가.

--- app/src/main/java/com/study/rev/androidrevolution/SettingsFragment.kt
+++ app/src/main/java/com/study/rev/androidrevolution/SettingsFragment.kt
@@ -1,10 +1,14 @@
 package com.study.rev.androidrevolution
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_settings.*
 
 class SettingsFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +18,30 @@ class SettingsFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+    private fun signOut()
+    {
+        removeLoginData()
+        var intent = Intent(this.activity, LoginActivity::class.java)
+        startActivity(intent)
+        this.activity?.finish()
+    }
+
+    private fun removeLoginData()
+    {
+        var pref = this.activity?.getSharedPreferences(LoginActivity.KEY_LOGIN_PREFERENCE, Context.MODE_PRIVATE)
+        var editor : SharedPreferences.Editor = pref!!.edit()
+
+        editor.clear()
+        editor.commit()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnSignOut.setOnClickListener {
+            signOut()
+        }
     }
 }
