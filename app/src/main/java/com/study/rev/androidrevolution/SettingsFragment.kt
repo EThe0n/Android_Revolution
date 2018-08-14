49a0f1c1f181d620128597141de20dd65618b068
Etheon <snow_chris@naver.com>
2018-08-14 16:57:52 +0900

    setting fragment 주석 처리

--- app/src/main/java/com/study/rev/androidrevolution/SettingsFragment.kt
+++ app/src/main/java/com/study/rev/androidrevolution/SettingsFragment.kt
@@ -1,9 +1,7 @@
 package com.study.rev.androidrevolution
 
 import android.app.AlertDialog
-import android.app.Dialog
 import android.content.Context
-import android.content.DialogInterface
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
@@ -16,20 +14,40 @@ import kotlinx.android.synthetic.main.fragment_settings.*
 import kotlinx.android.synthetic.main.nav_header_main.view.*
 import android.view.inputmethod.InputMethodManager
 
+/**
+ * 설정을 담당하는 프래그먼트
+ * 주로 상태메세지 설정 및 로그아웃 기능을 담당하고 있다.
+ *
+ * @constructor Default constructor
+ */
 class SettingsFragment : Fragment() {
+
     companion object {
+        /**
+         * Shared Preference에 있는 상태 메세지 정보를 가져오기 위한 키
+         */
         const val KEY_STATUS_MESSAGE : String = "STATUS_MESSAGE"
     }
 
+    /**
+     *
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         activity?.title = NavigationDrawerConstants.TAG_SETTINGS
     }
 
+    /**
+     *
+     */
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_settings, container, false)
     }
 
+    /**
+     * 로그아웃 시 실행되는 함수
+     * 해당 프래그먼트가 있는 액티비티를 종료하고 로그인 액티비티로 이동한다
+     */
     private fun signOut()
     {
         removeLoginData()
@@ -38,6 +56,9 @@ class SettingsFragment : Fragment() {
         this.activity?.finish()
     }
 
+    /**
+     * Shared Preference에 있는 로그인 데이터를 지울 때 사용되는 함수
+     */
     private fun removeLoginData()
     {
         var pref = this.activity?.getSharedPreferences(LoginActivity.KEY_LOGIN_PREFERENCE, Context.MODE_PRIVATE)
@@ -47,7 +68,11 @@ class SettingsFragment : Fragment() {
         editor.commit()
     }
 
-    // 상태 메세지를 데이터에 저장함
+    /**
+     * 상태메세지를 Shared Preference에 저장함
+     *
+     * @param msg 저장할 상태메세지
+     */
     private fun saveStatusMessage(msg : String)
     {
         var pref = this.activity?.getSharedPreferences(LoginActivity.KEY_LOGIN_PREFERENCE, Context.MODE_PRIVATE)
@@ -57,7 +82,9 @@ class SettingsFragment : Fragment() {
         editor.commit()
     }
 
-    // 입력 시에 나온 키보드를 내리기 위한 함수
+    /**
+     * 입력시에 나온 키보드를 내리는 함수
+     */
     private fun hideKeyboard()
     {
         try {
@@ -68,6 +95,9 @@ class SettingsFragment : Fragment() {
         }
     }
 
+    /**
+     *
+     */
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -104,4 +134,5 @@ class SettingsFragment : Fragment() {
             }
         }
     }
+
 }
