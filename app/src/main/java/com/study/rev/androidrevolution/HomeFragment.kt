8c07fc8b1dd3ae2106c3963dd8ccc2feb9a36ae2
Etheon <snow_chris@naver.com>
2018-08-16 13:30:18 +0900

    home fragment에 기능추가 1. 주소 클릭시 클립보드에 복사 2. 네이버 지도 링크에 연결

--- app/src/main/java/com/study/rev/androidrevolution/HomeFragment.kt
+++ app/src/main/java/com/study/rev/androidrevolution/HomeFragment.kt
@@ -1,10 +1,17 @@
 package com.study.rev.androidrevolution
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +21,31 @@ class HomeFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    private fun copyToClipboard(str : String) {
+        var clipBoardManager : ClipboardManager = this.activity?.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        var clipData : ClipData = ClipData.newPlainText("Data", str)
+
+        clipBoardManager.primaryClip = clipData
+        Toast.makeText(activity?.applicationContext, "클립보드에 복사하였습니다.", Toast.LENGTH_SHORT)
+                .show()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        textRoadNameAddress.setOnClickListener {
+            copyToClipboard(resources.getString(R.string.club_road_name_address))
+        }
+
+        textAddress.setOnClickListener {
+            copyToClipboard(resources.getString(R.string.club_address))
+        }
+
+        map_link.setOnClickListener{
+            var intent : Intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://map.naver.com/?mapmode=0&lng=5d89ecc27c18e665ad945318c3a99d80&pinId=16338555&lat=27575ffaa043999863beec7db800f450&dlevel=12&enc=b64&pinType=site"))
+            startActivity(intent)
+        }
     }
 }
